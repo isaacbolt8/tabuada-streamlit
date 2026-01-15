99f3b115b8fc25b88a97477f0036b144815532e1
isaacbolt8 <is4890547@gmail.com>
2026-01-15 00:38:19 -0300

    Create app.py
    primeiro app

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(page_title="Tabuada Inteligente", layout="centered")
+
+st.title("📘 Tabuada Inteligente")
+
+numero = st.number_input(
+    "Digite um número para ver a tabuada:",
+    min_value=0,
+    max_value=100,
+    step=1
+)
+
+st.divider()
+
+for i in range(1, 11):
+    st.write(f"{numero} x {i} = {numero * i}")
